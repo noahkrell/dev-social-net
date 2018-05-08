07126766610efd69554563efa53e2cf5e20c1d3d
Noah Krell <noahkrell14@gmail.com>
2018-05-08 15:01:51 -0500

    bring in validator and add name length validation on registration

---
+++ validation/is-empty.js
@@ -0,0 +1,12 @@
+const isEmpty = value => {
+  return (
+    value === undefined ||
+    value === null ||
+    (typeof value === "object" && Object.keys.length === 0) ||
+    (typeof value === "string" && value.trim().length === 0)
+  );
+};
+
+// could get rid of brackets and return declaration since it's an arrow function with one line of output
+
+module.exports = isEmpty;
