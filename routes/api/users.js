c978a5861cda20c70173ba4dac5748dc2809c73b
Noah Krell <noahkrell14@gmail.com>
2018-05-08 13:48:38 -0500

    Add login post route and basic logic for user login (email check and password compare)

--- routes/api/users.js
+++ routes/api/users.js
@@ -51,4 +51,31 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route GET api/users/login
+// @desc Login User / Return JWT token
+// @access Public
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find the user by email
+  User.findOne({ email }) // same as {email: email} in JS6
+    .then(user => {
+      // check for User
+      if (!user) {
+        return res.status(404).json({ email: "User not found" });
+      }
+
+      // check Password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          res.json({ msg: "Success", user });
+        } else {
+          return res.status(400).json({ password: "Password Incorrect" });
+        }
+      });
+    });
+});
+
 module.exports = router;
