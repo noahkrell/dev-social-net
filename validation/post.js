030e62da3bd733661b13e164e72f1f100ad63e95
Noah Krell <noahkrell14@gmail.com>
2018-05-09 12:37:33 -0500

    set up CREATE route for post

---
+++ validation/post.js
@@ -0,0 +1,19 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+  data.text = !isEmpty(data.text) ? data.text : "";
+
+  if (!Validator.isLength(data.text, { min: 2, max: 500 })) {
+    errors.text = "Post must be between 2 and 500 characters";
+  }
+  if (Validator.isEmpty(data.text)) {
+    errors.text = "Text field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
