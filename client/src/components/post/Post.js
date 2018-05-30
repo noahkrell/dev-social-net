f44ddf9afe776093aa7102915674e596cdf2449d
Noah Krell <noahkrell14@gmail.com>
2018-05-30 14:18:40 -0500

    add/delete comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import PostItem from "../posts/PostItem";
 import Spinner from "../common/Spinner";
 import CommentForm from "./CommentForm";
+import CommentFeed from "./CommentFeed";
 import { getPost } from "../../actions/postActions";
 import { Link } from "react-router-dom";
 
@@ -22,6 +23,7 @@ class Post extends Component {
         <div>
           <PostItem post={post} showActions={false} />
           <CommentForm postId={post._id} />
+          <CommentFeed postId={post._id} comments={post.comments} />
         </div>
       );
     }
