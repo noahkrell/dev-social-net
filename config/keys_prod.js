714bf6b3cd3ef734276b0db68245b6e20525b116
Noah Krell <noahkrell14@gmail.com>
2018-05-30 16:35:59 -0500

    cleaning up

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,6 +1,4 @@
-// way of making this object available outside of this file
 module.exports = {
-  // connect to mLab db using user/pass that you initiated ("noah:noah")
   mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY
 };
